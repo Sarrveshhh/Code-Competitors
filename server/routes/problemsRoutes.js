d7ae9886daff1cead9bd5ca55f94e9b264916d70
Sarrveshhh <sarrvesharyaa@gmail.com>
2023-05-28 13:06:47 +0530

    created submission endpoint and added necessary backend code

--- server/routes/problemsRoutes.js
+++ server/routes/problemsRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { problems } from "../index.js";
+import { problems, submissions } from "../index.js";
 
+
+//get all problems.
 router.get("/all", (req, res) => {
     const filteredProblem = problems.map(x => ({
         problemId: x.problemId,
@@ -15,6 +17,44 @@ router.get("/all", (req, res) => {
     
 });
 
+
+//get one problem complete description.
+router.post("/eachProblem/:id", (req, res) => {
+    const problemId = req.params.id;
+
+    const filteredProblem = problems.filter(x => x.problemId === problemId);
+
+    if(filteredProblem.length == 0){
+        
+        res.status(404).json({error:"Problem not found!"});
+    }
+    else{
+        res.status(200).json(filteredProblem);
+    }
+});
+
+
+//submit solution.
+router.post("/submission", (req, res) => {
+    const isCorrect = Math.random() < 0.5;
+    const problemIdd = req.body.problemIdd;
+    const solution = req.body.solution;
+    const userId = "Sarrvesh";
+
+    if(isCorrect){
+        res.status(200).json({"message":"Correct Solution!"});
+        const status = "AC";
+        submissions.push({problemIdd, isCorrect, solution, userId, status});
+    }
+    else{
+        res.status(200).json({"message":"Wrong Answer"});
+        const status = "WA";
+        submissions.push({problemIdd, isCorrect, solution, userId, status});
+    }
+
+    console.log(submissions);
+
+});
 
 
 
